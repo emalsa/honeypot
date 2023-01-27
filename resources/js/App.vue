a5967d355f754eb853145e16290b26ffe62f059f
dni <daniele@nicastro.io>
2023-01-27 17:28:09 +0100

    Init

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Navigation />
+  <router-view />
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  name: 'App',
+  components: {Navigation},
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="p-4">HEADER</h1>
+  <div class="p-4">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
+<style scoped>
+h1 {
+  background-color: cornflowerblue;
+  color: white;
+}
+</style>
