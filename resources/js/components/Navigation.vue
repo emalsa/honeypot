455121393f68e76b15935494564a737e00cacc13
dni <daniele@nicastro.io>
2023-02-05 13:29:56 +0100

    remove img

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -89,13 +89,15 @@
               </div>
               <div class="w-auto p-2">
                 <a class="inline-block navbar-burger" href="#">
-                  <svg width="24" height="24" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 18L18 6M6 6L18 18"
-                          stroke="#ffffff"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"></path>
-                  </svg>
+                  <div>
+                    <svg width="24" height="24" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6 18L18 6M6 6L18 18"
+                            stroke="#ffffff"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"></path>
+                    </svg>
+                  </div>
                 </a>
               </div>
             </div>
