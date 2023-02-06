25f02c92b33b25e1231aadfc09a8626b604d97da
dni <daniele@nicastro.io>
2023-02-06 23:40:07 +0100

    add fallback

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Page title</title>
+    <title>bitcoin-holder.org</title>
     <meta name='robots' content='noindex,follow'/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
